0fb2c0312f3a5f5c3e3f2c2ad80d480513a2e875
Роман Данилевич <dwordchanel@gmail.com>
2020-05-18 16:03:59 +0300

    Add project files.

---
+++ OOP_lab_2_2_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP_lab_2_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+
+            while (x <= 6)
+            {
+                double y = Math.Log10(x);
+
+                Console.WriteLine("x = {0:f1}, y = {1:f4}", x, y);
+
+                x += 0.2;
+            }
+        }
+    }
+}
